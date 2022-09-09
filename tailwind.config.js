1e2e3a87c7f1c86a9ee3f65589eebd204747f50d
migsilva89 <migdrum@gmail.com>
2022-09-09 18:26:08 +0100

    header and nav bar mobile

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'grayy':'hsl(0, 0%, 75%)',
+        'grayishViolet':'hsl(257, 7%, 63%)',
+        'veryDarkBlue':'hsl(255, 11%, 22%)',
+        'veryDarkViolet':'hsl(260, 8%, 14%)',
+        'cyann':'hsl(180, 66%, 49%)',
+        'darkViolet':'hsl(257, 27%, 26%)',
+      },
+      fontFamily: {
+        'poppins': ["'Poppins', sans-serif"],
+      },
+    },
   },
   plugins: [],
 }
